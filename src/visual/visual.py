1e06c3599159b9c62e8db2b44027fa5a03d7f34e
Muyao <2350076251@qq.com>
2025-01-12 23:23:42 +0800

    fix readme

--- src/visual/visual.py
+++ src/visual/visual.py
@@ -3,10 +3,10 @@ from src.utils import file_utils
 import pandas as pd
 
 
-def compare_multi_methods(task_name:str,sub_task:str,log_fold:PosixPath=Path("logs"),):
+def compare_multi_methods(task_name:str,model_name:str,sub_task:str,log_fold:PosixPath=Path("logs"),):
     log_path = log_fold / task_name 
     methods_name = [child.name for child in log_path.iterdir() if child.is_dir()]   
-    methods_paths = {child.name: child/f"{sub_task}.jsonl" for child in log_path.iterdir() if child.is_dir()}
+    methods_paths = {child.name: child / model_name / f"{sub_task}.jsonl" for child in log_path.iterdir() if child.is_dir()}
     score_board = {method_name: {} for method_name in methods_name}
     for method_name,method_path in methods_paths.items():
         method_file = file_utils.load_jsonl(method_path)
@@ -17,7 +17,23 @@ def compare_multi_methods(task_name:str,sub_task:str,log_fold:PosixPath=Path("lo
     
     df = pd.DataFrame.from_dict(score_board, orient='index')
     df['total'] = df.mean(axis=1) 
+    print(model_name)
     print(df)
+    return df
+    
+def show_task(task_name:str,sub_task:str,log_fold:PosixPath=Path("logs"),):
+    model_names = [
+        "DeepSeek-V3",
+        "Llama3-8B-Chinese-Chat",
+        "qwen2-vl-72b-instruct",
+    ]
+    for model_name in model_names:
+        df = compare_multi_methods(task_name,model_name,sub_task,log_fold)
+    return
     
 if __name__ == "__main__":
-    compare_multi_methods("gaokao_obj","2010-2022_Chemistry_MCQs-single_choice")
+    # 2010-2022_Geography_MCQs-multi_question_choice
+    # 2010-2022_Chemistry_MCQs-single_choice
+    # 2010-2022_Physics_MCQs-multi_choice
+    
+    show_task("gaokao_obj","2010-2022_Chemistry_MCQs-single_choice")
